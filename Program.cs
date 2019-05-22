4019f1b0f3cd97003720ae9826e53dcfdf28a704
richard.sobreiro <richard.sobreiro@gmail.com>
2019-05-21 22:36:15 -0300

    DASDSADA

--- Program.cs
+++ Program.cs
@@ -63,11 +63,157 @@ public class ParetoFrontier
             INumVar[] tfb = new INumVar[qViagens];
             INumVar[] hcc = new INumVar[qViagens];
 
-            INumVar[][][] x;
+            INumVar[][][] x = new INumVar[qViagens][][];
+            for (var i = 0; i < qViagens; i++)
+            {
+                for (var j = 0; j < qViagens; j++)
+                {
+                    x[i][j] = cplex.BoolVarArray(qPontosCarga);
+                }
+            }
+            INumVar[][][] y = new INumVar[qViagens][][];
+            for (var i = 0; i < qViagens; i++)
+            {
+                for (var j = 0; j < qViagens; j++)
+                {
+                    x[i][j] = cplex.BoolVarArray(qBetoneiras);
+                }
+            }
+            INumVar[] atrc = new INumVar[qViagens];
+            INumVar[] avnc = new INumVar[qViagens];
+            INumVar[] atrp = new INumVar[qViagens];
+            INumVar[] avnp = new INumVar[qViagens];
+            INumVar[][] vp = new INumVar[qViagens][];
             for(var i = 0; i < qViagens; i++)
+            {
+                vp[i] = cplex.BoolVarArray(qPontosCarga);
+            }
+            INumVar[][] vb = new INumVar[qViagens][];
+            for (var i = 0; i < qViagens; i++)
+            {
+                vb[i] = cplex.BoolVarArray(qBetoneiras);
+            }
 
-            //dvar boolean y[I][I][B]; // Se a betoneira b atende a viagem j após a viagem i
+            //ViagemNaoPodeSucederElaMesmaNaPesagem:
+            //forall(i in I, j in I, k in K : i == j){
+            //    x[i][j][k] <= 0;
+            //}
+            for (int i = 0; i < qViagens; i++)
+            {
+                for (int j = 0; j < qViagens; j++)
+                {
+                    for (int k = 0; k < qViagens; k++)
+                    {
+                        if(i == j)
+                        {
+                            cplex.AddLe(0, x[i][j][k]);
+                        }
+                    }
+                }
+            }
+            //ViagemNaoPodeSucederElaMesmaNaBetoneira:
+            //forall(i in I, j in I, b in B : i == j){
+            //    y[i][j][b] <= 0;
+            //}
 
+            //TodaViagemDeveAntecederSucederNoMaximoAlgumaViagemNaPesagemExcetoAPrimeira:
+            //forall(p in K, v in I){
+            //    sum(i in I)(x[i][v][p]) <= 1;
+            //    sum(i in I)(x[v][i][p]) <= 1;
+            //    sum(i in I)(x[v][i][p]) <= sum(i in I)(x[i][v][p]);
+            //    sum(i in I)(x[v][i][p]) >= sum(i in I)(x[i][v][p]);
+            //}
+
+            //TodaViagemDeveAntecederSucederNoMaximoApenasUmaViagemEmUmaBetoneiraExcetoAPrimeira:
+            //forall(b in B, v in I) {
+            //    sum(i in I)(y[i][v][b]) <= 1;
+            //    sum(i in I)(y[v][i][b]) <= 1;
+            //    sum(i in I)(y[v][i][b]) <= sum(i in I)(y[i][v][b]);
+            //    sum(i in I)(y[v][i][b]) >= sum(i in I)(y[i][v][b]);
+            //}
+
+            //ViagemFicticia1AntecedeNoMaximoAlgumaViagemEmCadaPontoDeCarga:
+            //forall(p in K){
+            //    sum(v in I : v > 1)(x[1][v][p]) <= 1;
+            //}
+
+            //ViagemFicticia1AntecedeNoMaximoAlgumaViagemEmCadaBetoneira:
+            //forall(b in B) {
+            //    sum(v in I : v > 1)(y[1][v][b]) <= 1;
+            //}
+            //SequenciamentoDePesagemDeBetoneirasNoMesmoPontoDeCarga:
+            //forall(p in K, i in I, j in I : j > 1){
+            //    (M * (1 - (x[i][j][p]))) + tfp[j] >=
+            //        tfp[i] +
+            //        sum(p in K)(vp[j][p] * dp[j][p]);
+            //}
+            //GarantiaTempoDeVidaDoConcreto:
+            //forall(j in I){
+            //    hs[j] - tfp[j] <= tmaxvc[j];
+            //}
+            //SequenciamentoDoAtendimentoDeViagensPelaMesmaBetoneira:
+            //forall(b in B, i in I, j in I: j > 1){
+            //    (M * (1 - y[i][j][b])) + tfb[j] >=
+            //        tfb[i] +
+            //        sum(p in K)(vp[j][p] * (dp[j][p] + (2 * dv[j][p]))) +
+            //        td[j];
+
+            //    hcc[j] >= tfb[j] - td[j] - sum(p in K)(vp[j][p] * (dv[j][p] + dp[j][p]));
+            //    hcc[j] <= tfb[j] - td[j] - sum(p in K)(vp[j][p] * (dv[j][p] + dp[j][p]));
+            //}
+            //SeAViagemSucedeAlgumaViagemEmUmPontoDeCargaElaDeveSerAtribuidaAoPontoDeCarga:
+            //forall(v in I, p in K){
+            //    vp[v][p] >= sum(i in I)(x[i][v][p]);
+            //    //vp[v][p] <= sum(i in I)(x[i][v][p]);	
+            //}
+            //SeAViagemEhAtendidaPelaBetoneiraAposAlgumaViagemEssaViagemSoPodeSerAtendidaPorEssaBetoneira:
+            //forall(v in I, b in B){
+            //    vb[v][b] >= sum(i in I)(y[i][v][b]);
+            //    vb[v][b] <= sum(i in I)(y[i][v][b]);
+            //}
+            //AtribuicaoViagemPontoCarga:
+            //forall(v in I){
+            //    sum(p in K)(vp[v][p]) <= 1;
+            //}
+            //forall(p in K, b in B, v in I){
+            //    if (pb[p][b] == 1)
+            //    {
+            //        vb[v][b] + vp[v][p] <= 2 * pb[p][b];
+            //    }
+            //    else
+            //    {
+            //        vb[v][b] + vp[v][p] <= 1;
+            //    }
+            //}
+            //AtrasoAvancoDasViagens:
+            //forall(i in I){
+            //    sum(b in B)(vb[i][b]) >= sum(p in K)(vp[i][p]);
+            //    //sum(b in B)(vb[i][b]) <= sum(p in K)(vp[i][p]);
+
+            //    //sum(b in B)(vb[i][b]) <= sum(p in K)(vp[i][p]);
+
+            //    atrc[i] >= hcc[i] - hs[i];
+            //    atrc[i] <= hcc[i] - hs[i];
+
+            //    avnc[i] >= hs[i] - hcc[i];
+            //    avnc[i] <= hs[i] - hcc[i];
+
+            //    atrp[i] >= hcc[i] - sum(p in K)((dv[i][p] + dp[i][p]) * vp[i][p]) - tfp[i];
+            //    atrp[i] <= hcc[i] - sum(p in K)((dv[i][p] + dp[i][p]) * vp[i][p]) - tfp[i];
+
+            //    avnp[i] >= sum(p in K)((dv[i][p] + dp[i][p]) * vp[i][p]) + tfp[i] - hcc[i];
+            //    avnp[i] <= sum(p in K)((dv[i][p] + dp[i][p]) * vp[i][p]) + tfp[i] - hcc[i];
+            //}
+            //NaoNegatividadeDasVariaveisReais:
+            //forall(i in I){
+            //    atrc[i] >= 0;
+            //    avnc[i] >= 0;
+            //    atrp[i] >= 0;
+            //    avnp[i] >= 0;
+            //    tfp[i] >= 0;
+            //    tfb[i] >= 0;
+            //    hcc[i] >= 0;
+            //}
         }
         catch (ILOG.Concert.Exception exc)
         {
